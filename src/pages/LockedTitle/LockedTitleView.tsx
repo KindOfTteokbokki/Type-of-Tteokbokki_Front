79f164d2b95187e8a5a5559b8a6c56ed0cdd17c4
coding-frog117 <syeshe2@gmail.com>
2024-02-24 20:20:56 +0900

    :white_check_mark: Test : locked-title 페이지 헤더 렌더링 테스트 통과

--- src/pages/LockedTitle/LockedTitleView.tsx
+++ src/pages/LockedTitle/LockedTitleView.tsx
@@ -4,6 +4,7 @@ import Navigation from '../../components/Navigation/Navigation';
 import * as S from './style';
 import { Icon } from '../Title/style';
 import lock from '../../assets/아이콘_나도추천할래/뒤로가기.svg';
+import { constants } from '../../constants/constants';
 
 interface Props {
 	onClickMoveBack: () => void;
@@ -14,6 +15,10 @@ export default function LockedTitleView(props: Props) {
 		<>
 			<NavLayout>
 				<Icon src={lock} onClick={props.onClickMoveBack} />
+				<S.Header>
+					<S.H1>{constants.LOCKED_TITLE.header[0]}</S.H1>
+					<S.H1>{constants.LOCKED_TITLE.header[1]}</S.H1>
+				</S.Header>
 			</NavLayout>
 			<Navigation />
 		</>
