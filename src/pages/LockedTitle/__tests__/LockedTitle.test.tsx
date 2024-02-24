cf1532d8bf04d15bf9cfa9bdf682e5c06580eed9
coding-frog117 <syeshe2@gmail.com>
2024-02-24 20:24:25 +0900

    :test_tube: Test : 받으러 가기 버튼 렌더링 실패하는 테스트 작성

--- src/pages/LockedTitle/__tests__/LockedTitle.test.tsx
+++ src/pages/LockedTitle/__tests__/LockedTitle.test.tsx
@@ -11,10 +11,10 @@ jest.mock('react-router-dom', () => ({
 const mockNavigate = jest.fn();
 (useNavigate as jest.Mock).mockReturnValue(mockNavigate);
 
+const onClickMoveBack = jest.fn();
+
 describe('LockedTitle test', () => {
 	it('헤더 텍스트 렌더링 테스트', () => {
-		const onClickMoveBack = jest.fn();
-
 		render(<LockedTitleView onClickMoveBack={onClickMoveBack} />);
 
 		const elementFirst = screen.getByText('아직 못 받은');
@@ -22,5 +22,13 @@ describe('LockedTitle test', () => {
 
 		expect(elementFirst).toBeInTheDocument();
 		expect(elementSecond).toBeInTheDocument();
+	});
+
+	it('받으러 가기 버튼 렌더링 테스트', () => {
+		render(<LockedTitleView onClickMoveBack={onClickMoveBack} />);
+
+		const element = screen.getByText('받으러 가기!');
+
+		expect(element).toBeInTheDocument();
 	});
 });
