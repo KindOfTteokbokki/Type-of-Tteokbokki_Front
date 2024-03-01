4a84d11ad1200614d1089f9f3e430c0c99d45d5a
coding-frog117 <syeshe2@gmail.com>
2024-03-01 15:49:36 +0900

    :sparkles: Feat : 내입맛 상세 페이지 데이터 바인딩

--- src/pages/MyTaste/MyTaste.tsx
+++ src/pages/MyTaste/MyTaste.tsx
@@ -4,12 +4,13 @@ import { constants } from '../../constants/constants';
 import { useNavigate, useParams } from 'react-router-dom';
 import { usePost } from '../../api/useFetch';
 import { baseUrl } from '../../api/useAxios';
+import { Action } from '../../slice/findStoreSlice';
 
 export default function MyTaste() {
 	const postFunc = usePost(`${baseUrl}/viewOneFromMyTaste`);
 
 	const { id } = useParams();
-	const [data, setData] = useState(['']);
+	const [data, setData] = useState<any>([]);
 	const navigator = useNavigate();
 	const header = constants.MY_TASTE.header;
 	const onClickBack = () => {
@@ -17,10 +18,10 @@ export default function MyTaste() {
 	};
 
 	useEffect(() => {
-		postFunc({ store_seq: id }).then((res) => {
-			console.log(res);
+		postFunc({ store_seq: id }).then((res: any) => {
+			setData(res.data);
 		});
 	}, []);
 
-	return <MyTasteView header={header} onClickBack={onClickBack} />;
+	return <MyTasteView header={header} onClickBack={onClickBack} data={data} />;
 }
