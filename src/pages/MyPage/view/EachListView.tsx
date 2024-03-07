339403bdce51af0aa3258656e272a914cbc78635
coding-frog117 <syeshe2@gmail.com>
2024-03-07 19:07:29 +0900

    :bug: Bug : 모바일에서 뷰 제대로 안보이는 문제 해결

--- src/pages/MyPage/view/EachListView.tsx
+++ src/pages/MyPage/view/EachListView.tsx
@@ -37,7 +37,7 @@ export default function EachListView(props: EachListProps) {
 						? props.item.file_path + props.item.file_masking_name
 						: basicReviewImg
 				}
-				alt={props.item.file_masking_name ? '떡볶이 사진' : '떡볶이 캐릭터 아이콘 사진(기본 이미지)'}
+				alt={props.item.file_masking_name ? '떡볶이 사진' : '가래떡 캐릭터 아이콘 사진(기본 이미지)'}
 				onClick={props.onClickMoveDetail}
 			/>
 			<S.ReviewText onClick={props.onClickMoveDetail}>{props.item.content}</S.ReviewText>
